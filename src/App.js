536ae3a08e65b03721006953693356f0d0bea52b
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-10-05 17:16:04 +0200

    Exemples de tailles égales

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import ExempleSubmit from './Exemples/Submit';
 import ExempleInitialValues from './Exemples/InitialValues';
 import ExempleSynchronousValidation from './Exemples/SynchronousValidation';
 import ExempleDispatchOnSubmit from './Exemples/DispatchOnSubmit';
+import ExempleFormValueSelector from './Exemples/FormValueSelector';
 
 class App extends Component {
   render() {
     return (
         <div style={{display: 'flex', margin:'50px', justifyContent:'space-around', flexWrap: 'wrap'}}>
-          <ExempleSubmit />
-          <ExempleInitialValues />
-          <ExempleSynchronousValidation />
-          <ExempleDispatchOnSubmit/>
+          { brique(<ExempleSubmit />) }
+          { brique(<ExempleInitialValues />) }
+          { brique(<ExempleSynchronousValidation />) }
+          { brique(<ExempleDispatchOnSubmit />) }
+          { brique(<ExempleFormValueSelector />) }
         </div>
     );
   }
 }
+const brique = (composant) => (
+    <div style={{width:'250px', padding:'10px', margin:'10px', border: '1px solid lightgray'}}>
+      {composant}
+    </div>
+)
 
 export default App
