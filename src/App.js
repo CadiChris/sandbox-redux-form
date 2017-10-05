b5ff96ea588d938e3732f71766736a2f08d7beb8
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-10-05 11:44:12 +0200

    Rangement par dossier "Example"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import FormAdresse from './FormAdresse';
+import ExempleSubmit from './ExempleSubmit/ExempleSubmit';
 
 class App extends Component {
   render() {
     return (
         <div style={{margin:'50px'}}>
-          <FormAdresse />
+          <ExempleSubmit />
         </div>
     );
   }
--- src/ExempleSubmit/ExempleSubmit.js
+++ src/ExempleSubmit/ExempleSubmit.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
-const FormAdresse = ({ handleSubmit }) => {
+const ExempleSubmit = ({ handleSubmit }) => {
   return (
     <div>
-      <h2>Adresse</h2>
+      <h2>Exemple Submit</h2>
       <form onSubmit={ handleSubmit }>
         <Field name="numero" component="input" type="text" placeholder="Numéro"/> <br/>
-        <Field name="rue" component="input" type="text" placeholder="Rue"/>
-
+        <Field name="rue" component="input" type="text" placeholder="Rue"/><br/>
         <button type="submit">Submit</button>
       </form>
     </div>
@@ -16,7 +15,7 @@ const FormAdresse = ({ handleSubmit }) => {
 }
 
 export default reduxForm({
-  form: 'Adresse',
+  form: 'ExempleSubmit',
   // onSubmit est définie ici, et sera appelée par ReduxForm. Pas besoin de le déclarer dans les props <FormAdresse />
   onSubmit: (values) => console.log('onSubmit !', values),
-})(FormAdresse)
+})(ExempleSubmit)
