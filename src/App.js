07157553f6872fca17daacaefd6be3f31e897e3c
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-10-05 11:59:32 +0200

    Exemple des valeurs initiales.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import ExempleSubmit from './ExempleSubmit/ExempleSubmit';
+import ExempleSubmit from './Exemples/Submit';
+import ExempleInitialValues from './Exemples/InitialValues';
 
 class App extends Component {
   render() {
     return (
-        <div style={{margin:'50px'}}>
+        <div style={{display: 'flex', margin:'50px', justifyContent:'space-around'}}>
           <ExempleSubmit />
+          <ExempleInitialValues />
         </div>
     );
   }
---
+++ src/Exemples/InitialValues.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+
+// C'est la prop initialValues qui sera utilisé par les Fields. Mais pas besoin de la déclarer
+const ExempleInitialValues = () => {
+  return (
+    <div>
+      <h2>Exemple Initial Values</h2>
+      <form>
+        <Field name="numero" component="input" type="text" placeholder="Numéro"/> <br/>
+        <Field name="rue" component="input" type="text" placeholder="Rue"/><br/>
+      </form>
+    </div>
+  )
+}
+
+// Dans le mapStateToProps on doit fournir une prop initialValues qui contient une valeur par champ.
+const mapStateToProps = (state) => ({
+  initialValues: {
+    // La clé JS doit correspondre au @name du <Field />
+    numero: state.app.compteur
+  }
+})
+
+export default connect(mapStateToProps)(reduxForm({
+  form: 'ExempleInitialValues',
+  // On pourrait aussi fournir directement l'initialValues ici, mais on préférera initialiser depuis le state.
+  // initialValues: {
+  //   numero: 9,
+  //   rue: "rue de Condé"
+  // }
+})(ExempleInitialValues))
